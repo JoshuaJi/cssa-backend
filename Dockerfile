2912f8d3af831d0f7a7b733a993e73bef3c7ac39
Xu Ji <xu.ji@ibm.com>
2018-04-04 09:30:39 -0400

    dockerfile port change

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,5 @@ RUN npm install
 # Bundle app source
 COPY . .
 
-EXPOSE 8081
+EXPOSE 3000
 CMD [ "npm", "start" ]
--- routes/application.js
+++ routes/application.js
@@ -22,26 +22,31 @@ router.post('/', function(req, res, next) {
        }
    });
 
+  var application_form = "Name: "+req.body.name+"\n\n"+
+  "Faculty: "+req.body.faculty+"\n\n"+
+  "Year: "+req.body.year+"\n\n"+
+  "Background: "+req.body.background+"\n\n"+
+  "Language(s): "+req.body.language+"\n\n"+
+  "Email: "+req.body.email+"\n\n"+
+  "Phone: "+req.body.phone+"\n\n"+
+  "1. What is the position that you intend to apply for? Please also indicate your second and third choice.: \n"+req.body.q1+"\n\n"+
+  "2. Why are you interested in applying for this position? How do you perceive the roles and objectives of CSSA within the McGill community?: \n"+req.body.q2+"\n\n"+
+  "3. What experience/qualifications do you possess that make you a good candidate for this position?: \n"+req.body.q3+"\n\n"+
+  "4. How do you envision fulfilling your role? What kind of ideas (ex. events, activities, initiatives, etc.) do you have in mind?: \n"+req.body.q4+"\n\n"+
+  "5. Do you have other talents/skills that you think will serve as an asset? (Such as photography, graphic and web design, sports and musical instruments…): \n"+req.body.q5+"\n\n"+
+  "6. Have you had any experience or been involved in another club or association within or outside of the McGill community?: \n"+req.body.q6+"\n\n";
+
   var mail = {
       from: "Xu Ji <jixu204@gmail.com>",
-      to: "joshuaji@icloud.com",
+      to: process.env.TO_EMAIL,
       subject: "CSSA Recruitment web application",
       text: "application from "+req.body.name,
-      html: "<b>application from "+req.body.name+
-            "<b>Name: </b>"+req.body.name+"</br>"+
-            "<b>Faculty: </b>"+req.body.faculty+"</br>"+
-            "<b>Year: </b>"+req.body.year+"</br>"+
-            "<b>Background: </b>"+req.body.background+"</br>"+
-            "<b>Language(s): </b>"+req.body.language+"</br>"+
-            "<b>Email: </b>"+req.body.email+"</br>"+
-            "<b>Phone: </b>"+req.body.phone+"</br>"+
-            "<b>1. What is the position that you intend to apply for? Please also indicate your second and third choice.: </b>"+req.body.q1+"</br>"+
-            "<b>2. Why are you interested in applying for this position? How do you perceive the roles and objectives of CSSA within the McGill community?: </b>"+req.body.q2+"</br>"+
-            "<b>3. What experience/qualifications do you possess that make you a good candidate for this position?: </b>"+req.body.q3+"</br>"+
-            "<b>4. How do you envision fulfilling your role? What kind of ideas (ex. events, activities, initiatives, etc.) do you have in mind?: </b>"+req.body.q4+"</br>"+
-            "<b>5. Do you have other talents/skills that you think will serve as an asset? (Such as photography, graphic and web design, sports and musical instruments…): </b>"+req.body.q5+"</br>"+
-            "<b>6. Have you had any experience or been involved in another club or association within or outside of the McGill community?: </b>"+req.body.q6+"</br>",
+      html: "<b>application from "+req.body.name,
       attachments: [
+        {
+          filename: req.body.name+".txt",
+          content: application_form
+        },
         {
           filename: req.files.fileuploadResume.name,
           content: req.files.fileuploadResume.data
